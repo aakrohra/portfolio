3ffe4aad05c67ec72df88fa7ae877e5ed7d312dc
aakrohra <aakrohra@gmail.com>
2025-12-05 22:43:32 -0500

    added in framings EPto music

--- src/pages/Music/InFramings.jsx
+++ src/pages/Music/InFramings.jsx
@@ -13,8 +13,20 @@ function InFramings() {
                     served as my main creative outlet and a chance to lead a project, connect with
                     a local community, and grow as both a musician and organizer. As the primary contact
                     for booking and events, I learned the value of professionalism, clear communication,
-                    and networking in a fast-moving scene.<br></br><br></br>Photo by Cole London. Edited by me.
+                    and networking in a fast-moving scene.
+                    
+                    <br></br>You can listen to the In Framings EP <i>Painted Over Portraits</i> below.<br></br><br></br>Photo by Cole London. Edited by me.
                 </p>
+            </div>
+            <div className="spotifyWrapper">
+                <iframe 
+                    src="https://open.spotify.com/embed/album/0Jo79HhLVmwsWeczODsuff?utm_source=generator"
+                    allow="encrypted-media"
+                    title="Derwin EP"
+                    frameBorder="0"
+                    width="450"
+                    height="380"
+                />
             </div>
         </Fade>
     )
