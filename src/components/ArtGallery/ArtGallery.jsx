a600e0762a14927a4c503c87d06b99dc0d8391dd
aakrohra <aakrohra@gmail.com>
2025-12-05 20:42:34 -0500

    added videos to art gallery

--- src/components/ArtGallery/ArtGallery.jsx
+++ src/components/ArtGallery/ArtGallery.jsx
@@ -1,15 +1,44 @@
 import './ArtGallery.css';
 
-const images = import.meta.glob('/src/assets/art/*.{jpg,jpeg,png}', { eager: true });
+const media = import.meta.glob('/src/assets/art/*.{jpg,jpeg,png,mp4}', { eager: true });
 
 function ArtGallery() {
     return (
         <div className="art-gallery">
-            {Object.values(images).map((img, idx) => (
-                <div className="art-item" key={idx}>
-                    <img src={img.default ?? img} width="1" height="1" alt={`Art ${idx + 1}`} />
-                </div>
-            ))}
+            {Object.entries(media).map(([path, file], idx) => {
+                const src = file.default ?? file;
+                const ext = path.split('.').pop().toLowerCase();
+                const isVideo = ['mp4'].includes(ext);
+
+                return (
+                    <div className="art-item" key={idx}>
+                        {isVideo ? (
+                            <div className="video-wrapper">
+                                <video
+                                    src={src}
+                                    muted
+                                    loop
+                                    playsInline
+                                    width="1"
+                                    height="1"
+                                    onMouseEnter={e => e.target.play()}
+                                    onMouseLeave={e => e.target.pause()} 
+                                    alt={`Art ${idx + 1}`}
+                               
+                                />
+                                <div className="play-icon">▶</div>
+                            </div>
+                        ) : (
+                            <img
+                                src={src}
+                                width="1"
+                                height="1"
+                                alt={`Art ${idx + 1}`}
+                            />
+                        )}
+                    </div>
+                );
+            })}
         </div>
     );
 }
